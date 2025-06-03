3b7913ce19785bba3c71b84ae41554a6383d2505
mincomk <mail@drchi.co.kr>
2025-06-04 04:33:40 +0900

    fix: multi return

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,7 +5,7 @@
  * Based on documentation from https://tweaked.cc/
  */
 
-export { };
+export {};
 
 /** @noSelf **/
 declare global {
@@ -241,7 +241,7 @@ declare global {
     function receive(
       protocolFilter?: string,
       timeout?: number,
-    ): [number, RednetData, string | null] | null;
+    ): LuaMultiReturn<[number, RednetData, string | null]> | null;
 
     /**
      * Register the system as "hosting" the desired protocol under the specified name. If a rednet lookup is performed for that protocol (and maybe name) on the same network, the registered system will automatically respond via a background process, hence providing the system performing the lookup with its ID number.
